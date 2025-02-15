de34b3c7b9ed5f55dde664673485f58eb35741f9
Dogor0555 <74686946+Dogor0555@users.noreply.github.com>
2025-02-15 06:37:20 -0600

    Ejercicio21

--- Ejercicio21/Program.cs
+++ Ejercicio21/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Ingrese el primer número: ");
+        int numero1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Ingrese el segundo número: ");
+        int numero2 = int.Parse(Console.ReadLine());
+
+        int suma = numero1 + numero2;
+        int resta = numero1 - numero2;
+        int multiplicacion = numero1 * numero2;
+        double division = (double)numero1 / numero2;
+
+        Console.WriteLine("La suma es: " + suma);
+        Console.WriteLine("La resta es: " + resta);
+        Console.WriteLine("La multiplicación es: " + multiplicacion);
+        Console.WriteLine("La división es: " + division);
+    }
+}
